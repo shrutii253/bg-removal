c40b99341d971006d57e7caf8b97e462b206f582
shrutii253 <saishrutipandey@gmail.com>
2025-05-11 15:48:10 +0530

    add userModel.js

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,5 +1,5 @@
 import { Webhook } from "svix"
-import userModel from "../models/usermodel.js"
+import userModel from "../models/userModel.js"
 
 //API controller function to manage clerk user with database
 //http://localhost:4000/api/user/webhooks
